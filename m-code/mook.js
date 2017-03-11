96d7d1de26e8b999d0c09b80e52897beda0ece75
penpagyaltsen <penpagyaltsen@192.168.0.13>
2017-03-11 10:02:01 -0500

    added env

--- m-code/mook.js
+++ m-code/mook.js
@@ -5,10 +5,12 @@ var express = require("express");
 
 var koo = express();
 
+// set evironment for deployment
+koo.set('port',process.env.PORT || 3000);
 koo.get('/',function(req,res){
 	res.send("hello world, Tashi Deleck! god bless us");
 });
 
-koo.listen(3000,function(){
-	console.log("started on on port 3000");
+koo.listen(koo.get('port'),function(){
+	console.log("started on on port", koo.get('port'));
 });
