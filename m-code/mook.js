5616be4424cc6c28784169ba829d21e3e53c7ddf
penpagyaltsen <penpagyaltsen@172.31.99.90>
2017-03-11 12:30:22 -0500

    first static files upload.

--- m-code/mook.js
+++ m-code/mook.js
@@ -2,13 +2,22 @@
  * http://usejsdoc.org/
  */
 var express = require("express");
-
+var path = require("path");
+var logger = require("morgan");
 var koo = express();
 
 // set evironment for deployment
 koo.set('port',process.env.PORT || 3000);
+koo.set('view engine','ejs');                         // set view engine as ejs
+koo.set('views',path.resolve(__dirname,'mockView')); // set view directory 
+
+// apply express static middleware for the static files
+koo.use(express.static(path.resolve(__dirname,'mockView')));
+koo.use(express.static(path.resolve(__dirname,'assets')));
+
+//set root router to render the home page.
 koo.get('/',function(req,res){
-	res.send("hello world, Tashi Deleck! god bless us");
+	res.render('mock-main');
 });
 
 koo.listen(koo.get('port'),function(){
